face8c3f47873d4e20df40d958280652ffc94538
AuthorRight <2238782426@qq.com>
2021-07-06 21:38:36 +0800

    项目构建

---
+++ bigData-server/src/test/java/com/bigdata/bigdataserver/BigDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.bigdataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

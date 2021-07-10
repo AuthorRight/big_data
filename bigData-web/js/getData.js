9684d6a1082585efce1e96fa030d2c36eec58226
AuthorRight <2238782426@qq.com>
2021-07-10 17:46:51 +0800

    前端项目交付

--- bigData-web/js/getData.js
+++ bigData-web/js/getData.js
@@ -1,4 +1,4 @@
-function getData(){
+function getData(){//未插入后端数据库数据时，调用新浪的api接口，使用本js文件
 	//获取疫情数据
 	$.ajax({
 		url:"https://news.sina.com.cn/project/fymap/ncp2020_full_data.json",
@@ -15,7 +15,24 @@ function getData(){
 			//分别存储当前确诊和累计确诊人数
 			var nowList = [];
 			var allList = [];
-			// console.log(list)
+			
+			//将数据插入表格
+			var table = document.getElementById("table-1");
+			for(var i = 0;i<list.length;i++){
+				var row = table.insertRow(table.rows.length);
+				var c1 = row.insertCell(0);//地区名
+				c1.innerHTML = list[i]["name"];
+				var c2 = row.insertCell(1);//新增
+				c2.innerHTML = list[i]["adddaily"]["conadd"];
+				var c3 = row.insertCell(2);//现有
+				c3.innerHTML = list[i]["econNum"];
+				var c4 = row.insertCell(3);//累计
+				c4.innerHTML = list[i]["value"];
+				var c5 = row.insertCell(4);//治愈
+				c5.innerHTML = list[i]["cureNum"];
+				var c6 = row.insertCell(5);//死亡
+				c6.innerHTML = list[i]["deathNum"];
+			}
 			
 			list.forEach(function(element){
 				nowList.push({
@@ -27,7 +44,7 @@ function getData(){
 					value:element.value
 				})
 			})
-			 //console.log(nowList)
+
 			 //console.log(allList)
 			 //获取境外输入 总新增确诊和当前日期的数据
 			var datas = [];//新增境外输入
@@ -42,6 +59,9 @@ function getData(){
 			var nowEconNum = [];//现有确诊人数
 			var cureNum = [];//治愈人数
 			var deathNum = [];//死亡人数
+			var cureRate = [];//治愈比例
+			var deathRate = [];//死亡比例
+			
 			for(var i in jwsrTop){
 				 jwsrTopProvince.push(jwsrTop[i].name);
 				 jwsrTopNum.push(jwsrTop[i].jwsrNum);
@@ -60,6 +80,8 @@ function getData(){
 				nowEconNum.push(historylist[i]["cn_econNum"])
 				cureNum.push(historylist[i]["cn_cureNum"])
 				deathNum.push(historylist[i]["cn_deathNum"])
+				cureRate.push(historylist[i]["cn_cureRate"])
+				deathRate.push(historylist[i]["cn_deathRate"])
 			 	if(i == 490){break}
 			}
 			 //将部分数据进行倒置
@@ -72,7 +94,10 @@ function getData(){
 			nowEconNum = nowEconNum.reverse();
 			cureNum = cureNum.reverse();
 			deathNum = deathNum.reverse();
-			//console.log(nowEconNum)
+			cureRate = cureRate.reverse();
+			deathRate = deathRate.reverse();
+			
+			console.log(cureRate)
 			//初始化echarts实例 渲染地图数据
 			var myChart3 = echarts.init(document.getElementById('mapEcharts'));
 			
@@ -179,6 +204,7 @@ function getData(){
 				var htmlStr = "";
 				for(var i in infoConfig){
 					htmlStr += `<li>
+
 							<h5>${infoConfig[i].title}</h5>
 							<p style="color:${infoConfig[i].color}">${historylist[0][i]}</p>
 							<span>
@@ -232,6 +258,7 @@ function getData(){
 					type:"line",
 					data:datas,
 					smooth:true,
+					color:"#7D64BC"
 				}]
 			};
 			myChart1.setOption(option1);
@@ -243,14 +270,14 @@ function getData(){
 				if(index == 0){//境外输入新增趋势
 					$("#modifyTitle").html("境外输入新增趋势");
 					option1.series[0].data = datas;
-					option1.series[0].color = "#476DA0";
+					option1.series[0].color = "#7D64BC";
 					option1.series[0].name = "新增境外输入";
 					myChart1.setOption(option1);
 				}
 				if(index == 1){//境外输入累计趋势
 					$("#modifyTitle").html("境外输入累计趋势");
 					option1.series[0].data = jwsrNum;
-					option1.series[0].color = "#B76767";
+					option1.series[0].color = "#19caad";
 					option1.series[0].name = "累计境外输入";
 					myChart1.setOption(option1);
 				}
@@ -267,7 +294,8 @@ function getData(){
 						series: [{
 							data: jwsrTopNum,
 							type: 'bar',
-							name:""
+							name:"",
+							color:"#29b7cb"
 						}]
 					};
 					myChart1.setOption(optionX);
@@ -320,12 +348,13 @@ function getData(){
 					type:"line",
 					data:datas,
 					smooth:true,
+					color:"#51c4d3"
 				},{
 					name:"总新增确诊",
 					type:"line",
 					data:addDatas,
 					smooth:true,
-					color:"#EB4F50"
+					color:"#efafad"
 				}]
 			};
 			myChart2.setOption(option2);
@@ -377,19 +406,19 @@ function getData(){
 					type:"line",
 					data:allConNum,
 					smooth:true,
-					color:"#EB4F50"
+					color:"#19caad"
 				},{
 					name:"现有疑似人数",
 					type:"line",
 					data:susNum,
 					smooth:true,
-					color:"#ED9825"
+					color:"#ef3473"
 				},{
 					name:"现有确诊人数",
 					type:"line",
 					data:nowEconNum,
 					smooth:true,
-					color:"#FE9C8F"
+					color:"#fcd337"
 				}]
 			};
 			myChart4.setOption(option4);
@@ -440,16 +469,73 @@ function getData(){
 					type:"line",
 					data:cureNum,
 					smooth:true,
-					color:"#91CC75"
+					color:"#57c3c2"
 				},{
 					name:"死亡",
 					type:"line",
 					data:deathNum,
 					smooth:true,
-					color:"#EB4F50"
+					color:"#fecc11"
 				}]
 			};
 			myChart5.setOption(option5);
+			
+			
+			//初始化echarts实例 全国 治愈率/死亡率 趋势
+			var myChart6 = echarts.init(document.getElementById('rateTendency'));
+			// 指定图表的配置项和数据
+			var option6 = {
+				//右侧图标设置
+				legend:{
+					itemWidth:12,
+					itemHeight:12,
+					right:20,
+					top:20,
+					orient:"horizontal",
+					textStyle:{
+						padding:[3,0,0,0]
+					}
+				},
+				//x轴属性
+				xAxis: {
+					type: 'category',
+					data: trendDate,
+					axisLabel:{
+						interval:25,
+						rotate:45,
+						color:"#ccc",
+						fontSize:10
+					}
+				},
+				//提示框
+				tooltip: {
+				    trigger: 'axis',
+				},
+				//y轴属性
+				yAxis: {  
+                        type: 'value',  
+                        axisLabel: {  //y轴百分比
+                            show: true,  
+                            interval: 'auto',  
+                            formatter: '{value} %'  
+                            },  
+                        show: true  
+                    }  ,
+				series:[{
+					name:"治愈率",
+					type:"line",
+					data:cureRate,
+					smooth:true,
+					color:"#10aec2"
+				},{
+					name:"死亡率",
+					type:"line",
+					data:deathRate,
+					smooth:true,
+					color:"#ed2f6a"
+				}]
+			};
+			myChart6.setOption(option6);
 		},
 	})
 }
